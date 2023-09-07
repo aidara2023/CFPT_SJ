bb8d1cd51dbcd7d9bc9eb8445c271d3520202eec
aidara2023 <bintou1212aidara@gmail.com>
2023-09-07 17:05:31 +0000

    creation table

---
+++ app/Models/Unite_de_formation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Unite_de_formation extends Model
+{
+    use HasFactory;
+}
