12eef50680cc5baad9611ccd68835819257c0251
ABAKAR SALEH BICHARA <saleyokor@gmail.com>
2023-09-11 12:35:44 +0200

    kelley : Modification : model de 10 à 20

--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Livre extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'id',
+        'titre_livre',
+        'id_categorie',
+        'id_auteur',
+         'id_edition'
+        
+
+    ];
 }
--- app/Models/Materiel.php
+++ app/Models/Materiel.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Materiel extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'id',
+        'Nom',
+        'date_entree',
+        'date_sortie',
+        'Etat',
+        'Quantité',
+        'id_service',
+        'id_salle',
+        'id_type_materiel',
+        'id_statut',
+        'id_unite_formation'
+        
+
+    ];
 }
