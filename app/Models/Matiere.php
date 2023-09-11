515330944c3f4574f2601ca39b991f1ab56d09e3
MauriceSarr <mauricesarr2016@gmail.com>
2023-09-11 12:24:23 +0000

    com1

--- app/Models/Matiere.php
+++ app/Models/Matiere.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Matiere extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'id',
+        'id'
+        'intitule'
+        ];
+
+
 }
