1c8aa8232348cb447a5ffedcc13a7ec6cc63cd50
aidara2023 <bintou1212aidara@gmail.com>
2023-09-12 13:03:43 +0000

    modif

---
+++ app/Http/Controllers/auteur/auteur_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\auteur;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class auteur_controller extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/editeur/editeur_view_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\editeur;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class editeur_view_controller extends Controller
+{
+    //
+}
