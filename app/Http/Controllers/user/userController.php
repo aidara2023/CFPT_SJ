60ab2fe6a901e5088650065e33874a5c3756b423
renauddjekornonde <99624430+renauddjekornonde@users.noreply.github.com>
2023-09-12 12:39:01 +0000

    update

--- app/Http/Controllers/user/userController.php
+++ app/Http/Controllers/user/userController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class userController extends Controller
 {
     public function index() {
