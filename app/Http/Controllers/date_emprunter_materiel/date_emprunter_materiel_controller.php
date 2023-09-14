90034b4dbb79e52e6deade70f08ba9d90eaf95e9
aidara2023 <bintou1212aidara@gmail.com>
2023-09-14 10:22:07 +0000

    validation

---
+++ app/Http/Controllers/date_emprunter_materiel/date_emprunter_materiel_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\date_emprunter_materiel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class date_emprunter_materiel_controller extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/financer_bourse/financer_bourse_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\financer_bourse;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class financer_bourse_controller extends Controller
+{
+    //
+}
