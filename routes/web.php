eb6c297ecf62ed0779381f1742e4adc6b5eebd9b
aidara2023 <122046394+aidara2023@users.noreply.github.com>
2023-10-24 12:52:20 +0000

    maachin

--- routes/web.php
+++ routes/web.php
@@ -108,6 +108,7 @@ use App\Http\Controllers\user\userViewController;
 
 
 use App\Http\Controllers\roleController;
+use App\Http\Controllers\surveillant\surveillant_view_controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -591,7 +592,12 @@ Route::put('retard/update/{$id}',[retard_controller::class,'update'])->name('ret
 Route::delete('retard/delete/{$id}',[retard_controller::class, 'delete'])->name('retard_delete');
 Route::get('retard/create' ,[retard_view_controller::class, 'create'])->name('retard_create');
 
-Route::get('create/service', [service_view_controller::class, 'create'])->name('create_service');
+Route::get('service/create', [service_view_controller::class, 'create'])->name('create_service');
 
-Route::get('/role/home',[roleController::class, 'index']);
+Route::get('/role/home',[role_controller::class, 'index']);
+
+//Surveillant
+Route::get('surveillant/index',[surveillant_view_controller::class, 'index'])->name('surveillant_index');
+
+
 
