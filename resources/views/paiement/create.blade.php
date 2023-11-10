021b8d8e105dff6e8752d1e29f14a69fe5281eee
aidara2023 <bintou1212aidara@gmail.com>
2023-11-10 17:23:34 +0000

    mis a jour

--- resources/views/paiement/create.blade.php
+++ resources/views/paiement/create.blade.php
@@ -1,29 +1,147 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assetsCFPT/css/cssCreate/create.css">
-    <title>Inscription</title>
-    @vite('resources/js/app.js')
-</head>
-<body id="app">
-    <div class="cote_gauche">
-        <div class="elements">
+@extends('layout.app')
 
-            <img src="/assetsCFPT/image/logo_cfpt_bleu.png" alt="">
-            <h1 class="titre">Enregistrement</h1>
-            <p>Veuillez remplir ce formulaire</p>
-        </div>
+@section('content')
+
+<div class="elements flou " id="app">
+
+    <div class="entete">
+        @include('layout.header')
     </div>
+
+
+    <paiement-liste></paiement-liste>
+
+    <!-- debut modal pour modifier utilisateur -->
+    <dialog data-modal-modification class="modal">
+        <h1>Modification</h1>
+        <div class="contenu">
+            <form action="" method="dialog" >
+                <h1 class="sous_titre">Informations Personnelles</h1>
+
+                <div class="personnel">
+                    <input type="text" name="nom" id="nom" placeholder="Nom">
+                    <input type="text" name="prenom" id="prenom" placeholder="Prenom">
+                    <input type="date" name="date_naissance" id="date_naissance" placeholder="Jour/Mois/Annee">
+                    <input type="text" name="lieu_naissance" id="lieu_naissance" placeholder="Lieu de Naissance">
+                </div>
+                <div class="sexe">
+                    <span class="b2">Sexe</span>
+                    <label for="masculin">Masculin
+                    <span></span>
+                        <input type="radio" name="sexe" id="masculin" value="masculin">
+                    </label>
+                    <label for="feminin">Feminin
+                    <span></span>
+                        <input type="radio" name="sexe" id="feminin" value="feminin" >
+                    </label>
+                </div>
+                <div class="num-addr">
+                    <input type="tel" name="telephone" id="telephone" placeholder="Tel : 77 234 48 43">
+                    <input type="text" name="adresse" id="adresse" placeholder="Adresse">
+                    <input type="text" name="nom_tuteur" id="nom_tuteur" placeholder="Nom tuteur">
+                    <input type="text" name="prenom_tuteur" id="prenom_tuteur" placeholder="Prénom tuteur">
+                    <input type="tel" name="telephone_tuteur" id="telephone_tuteur" placeholder="Telephone tuteur">
+                </div>
+
+                <p><span class="str">*</span> Personnes à contacter en cas d'urgence</p>
+                <div class="urgence">
+                    <input type="tel" name="contact_urgence_1" id="contact_urgence_1" placeholder="Contact d'urgence 1">
+                    <input type="tel" name="contact_urgence_2" id="contact_urgence_2" placeholder="Contact d'urgence 2">
+                </div>
+                <h1 class="sous_titre">Informations Académiques</h1>
+                <div class="academiques">
+                    <select name="niveau" id="niveau" aria-placeholder="Niveau">
+                        <optgroup label="BTI">
+                            <option value="">Niveau</option>
+                            <option value="">BTI</option>
+                            <option value="">BTI</option>
+                            <option value="">BTI</option>
+                        </optgroup>
+                        <optgroup label="BTS">
+                            <option value=""></option>
+                        </optgroup>
+                        <optgroup label="formations_continues">
+                            <option value=""></option>
+                        </optgroup>
+                    </select>
+
+                    <select name="classe" id="classe" placeholder="Niveau">
+                        <option value="">Classe</option>
+                        <option value="prive"> Privée</option>
+                        <option value="public"> Publique</option>
+                    </select>
+
+                    <select name="filiere" id="filiere" placeholder="Niveau">
+                        <optgroup label="Département Informatique">
+                            <option value="">Filiere</option>
+                            <option value="iir">Informatique Industrielle et Réseaux</option>
+                            <option value="auto">Automatique</option>
+                        </optgroup>
+                        <optgroup label="Département Mécanique">
+                            <option value=""></option>
+                        </optgroup>
+                        <optgroup label="Département Electrique">
+                            <option value=""></option>
+                        </optgroup>
+                    </select>
+
+                    <select name="annee_academique" id="annee_academique" placeholder="Niveau">
+                        <optgroup label="BTI">
+                            <option value="">Année académiques</option>
+                        </optgroup>
+                        <optgroup label="BTS">
+                            <option value=""></option>
+                        </optgroup>
+                        <optgroup label="formations_continues">
+                            <option value=""></option>
+                        </optgroup>
+                    </select>
+                </div>
+
+                <div class="boutons">
+                    <input  type="submit" class="texte" value="Ajouter">
+                </div>
+            </form>
+        </div>
+        <div class="boutons">
+            <button type="button" data-close-modal class="texte">Annuler</button>
+        </div>
+    </dialog>
+    <!-- {{--  Fin modal pour modifier utilisateur --}} -->
 
     <paiement-create></paiement-create>
     
 
-</body>
-</html>
+   <!--  {{--  Debut modal pour supprimer utilisateur --}} -->
+   <dialog data-modal-suppression class="modal">
+        <h1>Suppression</h1>
+        <div class="contenu">
+            <p>Etes vous sûr de vouloir supprimer cet utilisateur ?</p>
+        </div>
+        <div class="boutons">
+            <button type="button" data-close-modal class="texte">Annuler</button>
+            <input type="submit" value="Confirmer">
+        </div>
+    </dialog>
+   <!--  {{--  Fin modal pour supprimer utilisateur --}} -->
+
+
+    <dialog data-modal-confirmation class="modal">
+        <img src="../assetsCFPT/image/verified.gif" alt="" class="anime">
+        <h1>Réussi !</h1>
+        <br>
+        <p class="">Service ajouté avec succès</p>
+    </dialog>
+
+    <dialog data-modal-confirmation-sup class="modal">
+        <img src="../assetsCFPT/image/verified.gif" alt="" class="anime">
+        <h1>Réussi !</h1>
+        <br>
+        <p class="">Service supprimé avec succès</p>
+    </dialog>
+
+</div>
+
+<span class="fond"></span>
+    
+@endsection
