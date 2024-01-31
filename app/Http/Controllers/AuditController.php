6219e1bfec8639ec05faaaf4fb8ca77bbaa71638
renauddjekornonde <99624430+renauddjekornonde@users.noreply.github.com>
2024-01-31 20:29:38 +0000

    nouvelle mis a jour

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Audit;
 use Illuminate\Http\Request;
 
 class AuditController extends Controller
 {
-    //
+    public function index() {
+        $audit=Audit::with('user')->orderBy('created_at', 'desc')->get();
+        if($audit!=null){
+            return response()->json([
+                'statut'=>200,
+                'audit'=>$audit
+            ],200)  ;
+        }else{
+            return response()->json([ 
+                'statut'=>500,
+                'message'=>'Aucune donnée trouvée',
+            ],500 );
+        }
+     }
 }
