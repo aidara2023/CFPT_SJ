d80af801d3cc3fe11081d29a05eea55e42a73723
renauddjekornonde <99624430+renauddjekornonde@users.noreply.github.com>
2024-03-19 17:38:21 +0000

    nouvelle mis a jour

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -16,11 +16,11 @@ class Reservation extends Model
     ];
     
     public function salle (){
-        return $this->belongsTo(Salle::class,'id_salle');
+        return $this->belongsTo(Salle::class,'id_salle', 'id');
         
     }
     public function location (){
-        return $this->belongsTo(Location::class,'id_location');
+        return $this->belongsTo(Location::class,'id_location', 'id');
         
     }
 }
