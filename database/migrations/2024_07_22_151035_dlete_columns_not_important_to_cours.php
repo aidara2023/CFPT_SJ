3aa523748f5f9defc07d962a7437b584ee613449
renauddjekornonde <99624430+renauddjekornonde@users.noreply.github.com>
2024-07-31 15:09:44 +0000

    nouvelle mis a jour

--- database/migrations/2024_07_22_151035_dlete_columns_not_important_to_cours.php
+++ database/migrations/2024_07_22_151035_dlete_columns_not_important_to_cours.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('cours', function (Blueprint $table) {
-            $table->dropColumn('date_cour');
+        /*     $table->dropColumn('date_cour');
             $table->dropColumn('heure_debut');
-            $table->dropColumn('heure_fin');
+            $table->dropColumn('heure_fin'); */
            // $table->dropColumn('id_salle');
             $table->dropForeign(['id_salle']);
         });
@@ -26,10 +26,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('cours', function (Blueprint $table) {
-            $table->date('date_cour');
+         /*    $table->date('date_cour');
             $table->string('heure_debut');
-            $table->string('heure_fin');
-           // $table->foreign('id_salle')->references('id')->on('salles')->onDelete('cascade');
+            $table->string('heure_fin'); */
+           $table->foreign('id_salle')->references('id')->on('salles')->onDelete('cascade');
 
         });
     }
